22fd64871861a47f9070538458b349dcf86802a4
Chinmay Mudholkar <chinmay.mudholkar@outlook.com>
2024-04-08 19:32:24 +0100

    Updated tests

--- tests/test_5_patch.py
+++ tests/test_5_patch.py
@@ -19,14 +19,14 @@ class Test_Patch:
     @pytest.mark.order(501)
     @pytest.mark.patch
     def test_patch_001(self):
-        _expected = get_current_date_time()
+        _expected = get_current_date_time().split(sep="T")[0]
         _params = {"name": "morpheus", "job": "zion resident"}
         _endpoint = f"{self.config.get_base_url()}/api/users/2"
         _response = self.api.api_patch(endpoint=_endpoint, params=_params)
         assert _response.status_code == api_response_codes.OK, "Invalid response code"
         _response_json = json.loads(_response.text)
         _actual = _response_json["updatedAt"]
-        _actual = _actual.split(sep=".")[0]  # Don't verify the miliseconds
+        _actual = _actual.split(sep="T")[0]  # Don't verify the time
         assert (
             _expected == _actual
         ), f"Incorrect data in response.  Expected {_expected}, actual {_actual}."
